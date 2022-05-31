cbcabe7c9c7992a588dcbbd9d942883247c29b70
JunHao-k <jun_hao_koh@hotmail.com>
2022-05-31 08:33:15 +0000

    stuck at q9

--- q8_manipulating_dom_elements/script.js
+++ q8_manipulating_dom_elements/script.js
@@ -1,3 +1,34 @@
 // PLACE YOUR CODE HERE
+// # Q8 - Selecting Many Elements and Manipulation
 
+// Examine the HTML file, and perform the following manipulations:
 
+// 1. Select all elements with the class `emphasis` and change their font family to 'Verdana'
+
+// 2. Select all `<li class='finished'>` and put a strikethrough through them (google for
+// 'css text decoration')
+
+// 3. Select all the `<li class='todo'>` that are in `<ul id='low-priority'>` and
+// change their background color to blue
+
+// 4. Select all elements with the class `alert` and change the font color to red.
+
+let emphasisList = document.querySelectorAll('.emphasis')
+for(eachEmphasis of emphasisList){
+    eachEmphasis.style.fontFamily = "Verdana"
+}
+
+let finishList = document.querySelectorAll('li.finished')
+for(eachFinish of finishList){
+    eachFinish.style.textDecoration = "line-through"
+}
+
+let lowPriority = document.querySelectorAll('#low-priority li.todo')
+for(task of lowPriority){
+    task.style.backgroundColor = "blue"
+}
+
+let alertList = document.querySelectorAll('.alert')
+for(eachAlert of alertList){
+    eachAlert.style.backgroundColor = "red"
+}
--- q9_basic_form_manipulation/script.js
+++ q9_basic_form_manipulation/script.js
@@ -1,3 +1,15 @@
+// ## The question
+
+// Examine the form in `index.html` carefully, and answer the following questions in `script.js`. Answer all the questions
+// within the function.
+
+// 1. Extract out the email address from the form and store it in a variable named `email`
+
+// 2. Extract out the selected phone model from the form and store it in a variable named `model`
+
+// 3. Extract out the required services, and store it in a variable named `services`
+
+// 4. Extact out the selected pickup point and store it in a variable named `pickup`
 
 function getFormValues() {
 
@@ -8,6 +20,15 @@ function getFormValues() {
     let services = null;
 
     let pickup = null;
+
+    let submition = document.querySelector("#submit")
+    submition.addEventListener('click' , () => {
+        email = document.querySelector("#email-address").value
+        // console.log(document.querySelector("#email-address"))
+        model = document.querySelector('.phone-type').value
+        services = document.querySelector('.services').checked
+        pickup = document.querySelector("#pick-up").value
+    })
 
     // leave this line alone!
     return [email, model, services, pickup];
